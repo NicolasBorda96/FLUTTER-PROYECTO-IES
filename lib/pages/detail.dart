d1a5f80c9e4ce0fdda1b7c63cee50faf07c090b7
NicolasBorda96 <nicolas.borda@unillanos.edu.co>
2021-05-06 01:19:43 -0500

    Codigo base de las paginas

---
+++ lib/pages/detail.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class DetailPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Titulo"),
+      ),
+      body: _body(),
+    );
+  }
+
+  Widget _body() {
+    return Container();
+  }
+}
---
+++ lib/pages/search.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class SearchPage extends StatefulWidget {
+  @override
+  _SearchPageState createState() => _SearchPageState();
+}
+
+class _SearchPageState extends State<SearchPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Buscar por tipo"),
+      ),
+      body: _body(),
+    );
+  }
+
+  Widget _body() {
+    return Container();
+  }
+}
